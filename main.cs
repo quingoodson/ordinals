22f45af9e5eb8e5b960db490b34b5a7ecf3934a8
quingoodson <goodson109@student.pcsstn.net>
2020-01-10 15:28:08 +0000

    new program

--- main.cs
+++ main.cs
@@ -2,6 +2,42 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("ordinals");
+    
+    Console.WriteLine ("Max:");
+    string input = Consol.ReadLine();
+
+    try {
+      uint max = uint.Parse(input);
+
+      for (int i = 1; i <= max; i++){
+        if (i < = 0){
+          Console.WriteLine(i);
+          continue;
+        }
+        switch(i % 100){
+          case 11:
+          case 12:
+          case 13:
+          Console.WriteLine($"{i}th");
+        }
+        switch(i % 10){
+          case 1:
+          Console.WriteLine("$"{i}st);
+          continue;
+          case 2:
+          Console.WriteLine("$"{i}nd);
+          continue;
+          case 3:
+          Console.WriteLine("$"{i}rd);
+          continue;
+          default:
+          Console.WriteLine("$"{i}th);
+          continue;
+        }
+      }
+    }
+    catch(OverflowException){
+      Console.WriteLine($"{input} is out of range");
+    }
   }
 }
